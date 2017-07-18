37391eae87ba5bc31c90d4ba61ecd591b9bbfbfe
vongk <vongk@IT-SOFTWARE>
2017-07-18 10:56:44 +0700

    20170718v1

--- ManageAssets/Controllers/PaymentsController.cs
+++ ManageAssets/Controllers/PaymentsController.cs
@@ -33,23 +33,88 @@ namespace ManageAssets.Controllers
             }
             return View(lst);
         }
-        public ActionResult ExportPdf()
+
+        // GET: Default/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Default/Create
+        [HttpPost]
+        public ActionResult Create(PAYMENT pay)
+        {
+            // TODO: Add insert logic here
+            if (ModelState.IsValid)
+            {
+                db.PAYMENTS.Add(pay);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        // GET: Default/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Default/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Default/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult ExportPdf1()
         {
             return new ActionAsPdf("Details")
             {
                 FileName = Server.MapPath("~/Content/List.pdf")
             };
         }
-        public ActionResult ExportCustomers()
+        public ActionResult ExportPdf(string id)
         {
-            List<PAYMENT> allCustomer = db.PAYMENTS.ToList();
+            List<PAYMENT> allCustomer = db.PAYMENTS.Where(p=>p.Payment_ID == id).ToList();
 
-            var query = (from i in db.PAYMENTS
-                         select new
-                         {
-                             Content_VN = i.Content_CN ?? "a",
-                             Content_CN = i.Content_VN ?? "No Value"
-                         }).ToList();
+            //var query = (from i in db.PAYMENTS
+            //             select new
+            //             {
+            //                 Content_VN = i.Content_CN ?? "a",
+            //                 Content_CN = i.Content_VN ?? "No Value"
+            //             }).ToList();
 
             ReportDocument rd = new ReportDocument();
             rd.Load(Path.Combine(Server.MapPath("~/Reports"), "Payments.rpt"));
@@ -62,9 +127,33 @@ namespace ManageAssets.Controllers
 
             Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
             stream.Seek(0, SeekOrigin.Begin);
-            return File(stream, "application/pdf", "CustomerList.pdf");
+            return File(stream, "application/pdf", "Payments.pdf");
         }
 
+        public ActionResult ExportExcel(string id)
+        {
+            List<PAYMENT> allCustomer = db.PAYMENTS.Where(p=>p.Payment_ID == id).ToList();
+
+            //var query = (from i in db.PAYMENTS
+            //             select new
+            //             {
+            //                 Content_VN = i.Content_CN ?? "a",
+            //                 Content_CN = i.Content_VN ?? "No Value"
+            //             }).ToList();
+
+            ReportDocument rd = new ReportDocument();
+            rd.Load(Path.Combine(Server.MapPath("~/Reports"), "Payments.rpt"));
+
+            rd.SetDataSource(allCustomer);
+
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+
+            Stream stream = rd.ExportToStream(CrystalDecisions.Shared.ExportFormatType.Excel);
+            stream.Seek(0, SeekOrigin.Begin);
+            return File(stream, "application/xls", "Payments.xls");
+        }
 
     }
 }
